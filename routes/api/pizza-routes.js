9fd9908eeb4cc509d0cc1e7ea5ccf94d29963d0e
bonobo <arburpee@gmail.com>
2022-02-14 18:50:37 -0800

    added crud routes

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+//get all and post at /api/pizzas
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+//get one, put, and delete at /api/pizzas/id
+
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
