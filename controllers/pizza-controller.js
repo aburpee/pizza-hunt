b10f220b6235c0e207e0f7394025fa4b42aa001f
bonobo <arburpee@gmail.com>
2022-02-20 18:04:36 -0800

    deploying

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -49,7 +49,7 @@ const pizzaController = {
     },
     //PUT api/pizzas/id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true})
         .then(dbPizzaData => {
             if (!dbPizzaData) {
                 res.status(404).json({ message: 'no pizza found with this id'});
